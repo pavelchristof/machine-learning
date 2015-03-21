fb8abe0bae8b1f3237b3fd754916c1d0acc3c491
Paweł Nowak <pawel834@gmail.com>
2015-03-21 23:02:58 +0100

    Implement monoid homomorphism model using Compose.

--- src/Data/ML/Monoid.hs
+++ src/Data/ML/Monoid.hs
@@ -1,10 +1,8 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE RankNTypes #-}
 {- |
 Module      :  Data.ML.Monoid
@@ -24,14 +22,12 @@ import Control.Applicative
 import Data.Bytes.Serial
 import Data.Foldable
 import Data.Key
-import Data.ML.Domain
+import Data.ML.Internal.Compose
 import Data.ML.Matrix
 import Data.ML.Model
 import Data.Monoid
 import Data.Traversable
-import GHC.TypeLits
 import Linear
-import Prelude hiding (sum)
 
 -- | A free monoid over a, with an extra unused type parameter to match
 -- the kind of model input.
@@ -42,33 +38,16 @@ toFreeMonoid x = FreeMonoid (\f -> foldMap f x)
 
 -- | A homomorphism from the free monoid over the given domain to a monoid @m a@.
 newtype MonoidHomModel dom m a
-    = MonoidHomModel (dom (m a))
-    deriving (Functor, Foldable, Traversable)
-
-deriving instance (Show (dom (m a))) => Show (MonoidHomModel dom m a)
-
-instance (Applicative dom, Applicative m) => Applicative (MonoidHomModel dom m) where
-    pure = MonoidHomModel . pure . pure
-    MonoidHomModel f <*> MonoidHomModel x = MonoidHomModel
-        ((<*>) <$> f <*> x)
-
-instance (Additive m, Applicative dom) => Additive (MonoidHomModel dom m) where
-    zero = MonoidHomModel (pure zero)
-    liftU2 f (MonoidHomModel m) (MonoidHomModel m') =
-        MonoidHomModel (liftA2 (liftU2 f) m m')
-    liftI2 f (MonoidHomModel m) (MonoidHomModel m') =
-        MonoidHomModel (liftA2 (liftI2 f) m m')
-
-instance (Metric m, Applicative dom, Foldable dom) => Metric (MonoidHomModel dom m) where
-    dot (MonoidHomModel m) (MonoidHomModel m') = sum (dot <$> m <*> m')
+    = MonoidHomModel (Compose dom m a)
+    deriving (Show, Functor, Applicative, Foldable, Traversable, Additive, Metric)
 
 instance (Serial1 dom, Serial1 m) => Serial1 (MonoidHomModel dom m) where
-    serializeWith f (MonoidHomModel m) = serializeWith (serializeWith f) m
-    deserializeWith f = MonoidHomModel <$> deserializeWith (deserializeWith f)
+    serializeWith f (MonoidHomModel m) = serializeWith f m
+    deserializeWith f = MonoidHomModel <$> deserializeWith f
 
 instance (Indexable dom, Multiplicative m) => Model (MonoidHomModel dom m) where
     type Input (MonoidHomModel dom m) = FreeMonoid (Key dom)
     type Output (MonoidHomModel dom m) = m
 
-    predict (FreeMonoid input) (MonoidHomModel m) =
+    predict (FreeMonoid input) (MonoidHomModel (Compose m)) =
         getProduct1 $ input (Product1 . index m)
