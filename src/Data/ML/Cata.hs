548b7af577c8526687caf9b7e79d34483312eb57
Paweł Nowak <pawel834@gmail.com>
2015-04-05 19:10:46 +0200

    Refined the typeclasses in Data.ML.Cata.

--- src/Data/ML/Cata.hs
+++ src/Data/ML/Cata.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFunctor #-}
@@ -14,68 +16,98 @@ Stability   :  experimental
 -}
 module Data.ML.Cata where
 
+import Data.Bytes.Get
+import Data.Bytes.Put
 import Data.Bytes.Serial
 import Data.ML.Internal.Product
 import Data.ML.Model
+import GHC.Generics
 import Linear
+
+class HFunctor (n :: (* -> *) -> (* -> *)) where
+    hmap :: (g a -> h a) -> n g a -> n h a
+
+class Functor1 (n :: (* -> *) -> (* -> *)) where
+    map1 :: Functor g => (a -> b) -> n g a -> n g b
+
+class Foldable1 (n :: (* -> *) -> (* -> *)) where
+    foldHmap :: (Foldable g, Monoid m) => (a -> m) -> (n g a -> m)
+
+class HSerial1 (n :: (* -> *) -> * -> *) where
+    hserializeWith :: (MonadPut m, Serial1 f) => (a -> m ()) -> n f a -> m ()
+    hdeserializeWith :: (MonadGet m, Serial1 f) => m a -> m (n f a)
+
+-- | Higher order functor fixpoint.
+newtype Fix1 (f :: (* -> *) -> * -> *) (a :: *) = Fix1 (f (Fix1 f) a)
+    deriving (Generic, Generic1)
+
+instance Functor1 f => Functor (Fix1 f) where
+    fmap f (Fix1 x) = Fix1 (map1 f x)
+
+instance Foldable1 f => Foldable (Fix1 f) where
+    foldMap f (Fix1 x) = foldHmap f x
+
+instance HSerial1 f => Serial1 (Fix1 f) where
+    serializeWith f (Fix1 x) = hserializeWith f x
+    deserializeWith f = Fix1 <$> hdeserializeWith f
+
+cata1 :: HFunctor f => (f g a -> g a) -> Fix1 f a -> g a
+cata1 f (Fix1 x) = f (hmap (cata1 f) x)
 
 -- | Folds the fixpoint of f to g using a model m.
 --
 -- The model m should transform @f g@ to @g@.
 newtype Cata (f :: (* -> *) -> * -> *) (m :: * -> *) (a :: *) = Cata (m a)
-    deriving (Functor, Applicative, Foldable, Traversable, Additive, Metric)
+    deriving (Functor, Applicative, Foldable, Traversable, Additive, Metric, Generic1)
 
--- | Higher order functor fixpoint.
-newtype Fix1 (f :: (* -> *) -> * -> *) (a :: *) = Fix1 (f (Fix1 f) a)
+instance Serial1 m => Serial1 (Cata f m)
 
-cata1 :: Functor1 f => (f g a -> g a) -> Fix1 f a -> g a
-cata1 f (Fix1 x) = f (hlmap (cata1 f) x)
-
-instance Functor1 f => Functor (Fix1 f) where
-    fmap f (Fix1 x) = Fix1 (hrmap f x)
-
--- | Higher-order functor in the first parameter, usual functor in the second
--- parameter. The second parameter gets applies to the first one to form a
--- fully applied type. You can map over both parameters.
---
--- prop>
-class Functor1 (n :: (* -> *) -> (* -> *)) where
-    hlmap :: (g a -> h a) -> n g a -> n h a
-    hrmap :: Functor g => (a -> b) -> n g a -> n g b
-
-instance Serial1 m => Serial1 (Cata f m) where
-    serializeWith f (Cata m) = serializeWith f m
-    deserializeWith f = Cata <$> deserializeWith f
-
-instance (Functor1 f, Model m, Input m ~ f (Output m))
+instance (HFunctor f, Functor1 f, Model m, Input m ~ f (Output m))
          => Model (Cata f m) where
     type Input (Cata f m) = Fix1 f
     type Output (Cata f m) = Output m
-    predict (Fix1 x) m@(Cata m') = predict (hlmap (`predict` m) x) m'
+    predict (Fix1 x) m@(Cata m') = predict (hmap (`predict` m) x) m'
 
 -- | Binary tree suitable for use with the catamorphism model.
 data Tree g f a
     = Leaf (g a)
     | Node (f a) (f a)
+    deriving (Functor, Foldable, Traversable, Generic, Generic1)
 
-instance (Functor g, Functor f) => Functor (Tree g f) where
-    fmap f (Leaf x) = Leaf (fmap f x)
-    fmap f (Node l r) = Node (fmap f l) (fmap f r)
+instance (Serial (g a), Serial (f a), Serial a) => Serial (Tree g f a)
+instance (Serial1 g, Serial1 f) => Serial1 (Tree g f)
+instance Serial1 g => HSerial1 (Tree g) where
+    hserializeWith f (Leaf x) = do
+        putWord8 0
+        serializeWith f x
+    hserializeWith f (Node l r) = do
+        putWord8 1
+        serializeWith f l
+        serializeWith f r
+    hdeserializeWith f = do
+        i <- getWord8
+        case i of
+          0 -> Leaf <$> deserializeWith f
+          1 -> Node <$> deserializeWith f <*> deserializeWith f
+          _ -> error "invalid data"
+
+instance HFunctor (Tree g) where
+    hmap _ (Leaf g) = Leaf g
+    hmap f (Node l r) = Node (f l) (f r)
 
 instance Functor g => Functor1 (Tree g) where
-    hlmap _ (Leaf g) = Leaf g
-    hlmap f (Node l r) = Node (f l) (f r)
+    map1 f (Leaf g) = Leaf (fmap f g)
+    map1 f (Node l r) = Node (fmap f l) (fmap f r)
 
-    hrmap f (Leaf g) = Leaf (fmap f g)
-    hrmap f (Node l r) = Node (fmap f l) (fmap f r)
+instance Foldable g => Foldable1 (Tree g) where
+    foldHmap f (Leaf g) = foldMap f g
+    foldHmap f (Node l r) = foldMap f l `mappend` foldMap f r
 
 newtype TreeAlgebra (leaf :: * -> *) (node :: * -> *) a
     = TreeAlgebra (Product leaf node a)
-    deriving (Functor, Applicative, Foldable, Traversable, Additive, Metric)
+    deriving (Functor, Applicative, Foldable, Traversable, Additive, Metric, Generic1)
 
-instance (Serial1 leaf, Serial1 node) => Serial1 (TreeAlgebra leaf node) where
-    serializeWith f (TreeAlgebra m) = serializeWith f m
-    deserializeWith f = TreeAlgebra <$> deserializeWith f
+instance (Serial1 leaf, Serial1 node) => Serial1 (TreeAlgebra leaf node)
 
 instance ( Model leaf, Model node
          , Output node ~ Output leaf
@@ -85,3 +117,23 @@ instance ( Model leaf, Model node
     type Output (TreeAlgebra leaf node) = Output leaf
     predict (Leaf x) (TreeAlgebra (Pair leaf _)) = predict x leaf
     predict (Node l r) (TreeAlgebra (Pair _ node)) = predict (Pair l r) node
+
+newtype MapTree (f :: * -> *) (a :: *) = MapTree (f a)
+    deriving (Functor, Applicative, Foldable, Traversable, Additive, Metric, Generic1)
+
+instance Serial1 f => Serial1 (MapTree f)
+
+instance Model f => Model (MapTree f) where
+    type Input (MapTree f) = Fix1 (Tree (Input f))
+    type Output (MapTree f) = Fix1 (Tree (Output f))
+    predict (Fix1 (Leaf x)) (MapTree m) = Fix1 (Leaf (predict x m))
+    predict (Fix1 (Node l r)) m = Fix1 (Node (predict l m) (predict r m))
+
+listToTree :: g a -> [g a] -> Fix1 (Tree g) a
+listToTree leaf [] = Fix1 $ Leaf leaf
+listToTree leaf (x:xs) = Fix1 $ Node (Fix1 $ Leaf x) (listToTree leaf xs)
+
+listToTree1 :: [g a] -> Fix1 (Tree g) a
+listToTree1 [x] = Fix1 $ Leaf x
+listToTree1 (x:xs) = Fix1 $ Node (Fix1 $ Leaf x) (listToTree1 xs)
+listToTree1 _ = error "listToTree1: empty list"
